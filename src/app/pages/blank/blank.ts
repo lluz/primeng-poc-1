b5e485f8bfc34fa27978be9229b6ba260f18f846
uxd.agency <uxd.agency@gmail.com>
2026-01-05 01:43:54 +0000

    updated page blank

--- src/app/pages/blank/blank.ts
+++ src/app/pages/blank/blank.ts
@@ -10,19 +10,17 @@ import { RouterModule } from '@angular/router';
 	styleUrl: './blank.css',
 	template: `
     <div class="p-8 font-sans">
-        <p-drawer [(visible)]="visible" position="right" header="Right Drawer" styleClass="!w-80">
-            <nav class="flex flex-col gap-2">
-            <a routerLink="/" (click)="visible = false" class="p-3 hover:bg-surface-100 rounded text-surface-700 font-medium no-underline block">Home Page</a>
-          </nav>
-        </p-drawer>
-
         <div class="flex flex-col items-start gap-4">
-            <p class="text-lg text-slate-700">This is a blank page with no shell.</p>
-            <p-button label="Open Right Drawer" icon="pi pi-arrow-left" (onClick)="visible = true"></p-button>
+
+					<p class="text-lg text-slate-700">This is a blank page with no shell.</p>
+					
+					<nav class="flex flex-col gap-2">
+            <p-button routerLink="/" label="Home Page" icon="pi pi-arrow-left"></p-button>
+          </nav>
+
         </div>
     </div>
   `,
 })
 export class Blank {
-	visible: boolean = false;
 }
