c47f082022b01fdf654320efb51c99b1f9561852
uxd.agency <uxd.agency@gmail.com>
2026-01-05 03:16:41 +0000

    feat: add new blank page component with a home navigation button

--- src/app/pages/blank/blank.ts
+++ src/app/pages/blank/blank.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 import { DrawerModule } from 'primeng/drawer';
-import { ButtonModule } from 'primeng/button';
+import { MyButtonComponent } from '../../components/molecules/buttons/my-button';
 import { RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'app-blank',
 	standalone: true,
-	imports: [DrawerModule, ButtonModule, RouterModule],
+	imports: [DrawerModule, MyButtonComponent, RouterModule],
 	styleUrl: './blank.css',
 	template: `
     <div class="p-8 font-sans">
         <div class="flex flex-col items-start gap-4">
 
 					<p class="text-lg text-slate-700">This is a blank page with no shell.</p>
-					
+
 					<nav class="flex flex-col gap-2">
-            <p-button routerLink="/" label="Home Page" icon="pi pi-arrow-left"></p-button>
+						<app-my-button routerLink="/" label="Home Page" icon="pi pi-arrow-left"></app-my-button>
           </nav>
 
         </div>
